57d1980513b1474bb4fdeb8603a91e12cc244c41
Mateusz <mkubasz@gmail.com>
2021-10-11 21:37:15 +0200

    fix
    Feature/create hello world refs: create hello world function
    
    * chore: add gitignore for python app
    
    * feat: creat interfaces
    
    * feat: add logger
    
    * feat: add method to print hello world
    
    refactor: implement protocole
    
    refactor: wrap logger to class
    
    Added new method name and print

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from typing import Protocol
 
 
-def logger(text: str):
-    print(f'lolz ale fajny tekst: {text}')
-
-
 class Logger:
     def info(self, text: str):
         print(f'Logger: {text}')
@@ -21,6 +17,3 @@ class HelloWorld(Factory):
     def execute(self):
         logger.info('HelloWorld')
         print('Hello world')
-
-
-HelloWorld().execute()
